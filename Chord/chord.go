457af692d57b9eed882afaaed415188edb182713
Jared Nebeker <you@example.com>
2018-03-22 16:22:22 -0600

    fixed some prints

--- Chord/chord.go
+++ Chord/chord.go
@@ -6,14 +6,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	// "math"
 	"math/big"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
 	"strings"
-	// "strconv"
+	"strconv"
 	"time"
 )
 
@@ -28,6 +28,8 @@ const keySize = sha1.Size * 8
 const SuccessorsListSize = 3
 var two = big.NewInt(2)
 var hashMod = new(big.Int).Exp(big.NewInt(2), big.NewInt(keySize), nil)
+
+var force_strings = strings.Split("It looks like Sandpeople did this, all right. Look, here are Gaffi sticks, Bantha tracks. It's just...I never heard of them hitting anything this big before. They didn't. But we are meant to think they did. These tracks are side by side. Sandpeople always ride single file to hide there numbers. These are the same Jawas that sold us Artoo and Threepio. And these blast points, too accurate for Sandpeople. Only Imperial stormtroopers are so precise. Why would Imperial troops want to slaughter Jawas? If they traced the robots here, they may have learned who they sold them to. And that would lead them home! Wait, Luke! It's too dangerous. Uncle Owen! Aunt Beru! Uncle Owen! Hey, you're not permitted in there. It's restricted. You'll be deactivated for sure.. Don't call me a mindless philosopher, you overweight glob of grease! Now come out before somebody sees you. Secret mission? What plans? What are you talking about? I'm not getting in there! I'm going to regret this. There goes another one. Hold your fire. There are no life forms. It must have been short-circuited. That's funny, the damage doesn't look as bad from out here. Are you sure this things safe? Are they away? They have just made the jump into hyperspace. You're sure the homing beacon is secure aboard their ship? I'm taking an awful risk, Vader. This had better work. Hey...hey, open the pressure maintenance hatch on unit number... where are we? Three-two-six-eight-two-seven. If we can just avoid any more female advice, we ought to be able to get out of here. Well, let's get moving! Where are you going? No, wait. They'll hear! Come here, you big coward! Chewie! Come here! Listen. I don't know who you are, or where you came from, but from now on, you do as I tell you. Okay? Look, Your Worshipfulness, let's get one thing straight! I take orders from one person! Me! It's a wonder you're still alive. Will somebody get this big walking carpet out of my way? No reward is worth this. Well, that's the trick, isn't it? And it's going to cost you something extra. Ten thousand in advance. Ten thousand? We could almost buy our own ship for that! But who's going to fly it, kid! You? You bet I could. I'm not such a bad pilot myself! We don't have to sit here and listen... We haven't that much with us. But we could pay you two thousand now, plus fifteen when we reach Alderaan. Seventeen, huh! Okay. You guys got yourself a ship. We'll leave as soon as you're ready. Docking bay Ninety-four. Ninety-four. Looks like somebody's beginning to take an interest in your handiwork. All right, we'll check it out.", " ")
 
 func jump(address Address, fingerentry int) *big.Int {
 	n := hashString(address)
@@ -116,6 +118,10 @@ func (s Server) GetSuccessors(args *Nothing, reply *[]Address) error {
 func (n *Node) Stabilize() {
 	client, err := rpc.DialHTTP("tcp", string(n.successor))
 	if err != nil {
+		log.Printf("We have lost a successor")
+		for i := range(n.successors) {
+			log.Printf("%s", n.successors[i])
+		}
 		n.successors = n.successors[1:]
 		if len(n.successors) == 0 {
 			n.successor = n.me
@@ -128,7 +134,9 @@ func (n *Node) Stabilize() {
 	var predecessor Address
 	var junk Nothing
 	if err := client.Call("Server.GetPredecessor", &junk, &predecessor); err != nil {
-		log.Fatalf("Server.GetPredecessor: %v", err)
+		log.Printf("Server.GetPredecessor: %v", err)
+		closeClient(client);
+		return
 	}
 	closeClient(client)
 	if predecessor != "" {
@@ -141,15 +149,20 @@ func (n *Node) Stabilize() {
 	client, err = rpc.DialHTTP("tcp", string(n.successor))
 	if err != nil {
 		log.Printf("%v, %s : %s", n.successors, n.successor, n.predecessor)
+		closeClient(client)
 		return
 	}
 	if err := client.Call("Server.GetSuccessors", &junk, &n.successors); err != nil {
-		log.Fatalf("Server.GetSuccessors: %s", err)
+		log.Printf("Server.GetSuccessors: %s", err)
+		closeClient(client)
+		return
 	}
 	// log.Printf("after stablize getsuccessors")
 
 	if err := client.Call("Server.Notify", &n.me, &junk); err != nil {
-		log.Fatalf("Server.Notify: %v", err)
+		log.Printf("Server.Notify: %v", err)
+		closeClient(client)
+		return
 	}
 	closeClient(client)
 	// log.Printf("stablize/ ending")
@@ -218,7 +231,7 @@ func find(id *big.Int, start Address) Address {
 	findInfo.Found = false
 	findInfo.Address = start
 	i := 0
-	maxSteps := 160
+	maxSteps := 161
 	for !(findInfo.Found) && i < maxSteps {
 		client := getClient(findInfo.Address)
 		if err := client.Call("Server.Find_successor", id, &findInfo); err != nil {
@@ -344,6 +357,7 @@ func (s Server) Get_all(args *Address, reply *map[Key]string) error {
 		if len(*reply) == 0 {
 			*reply = make(map[Key]string)
 		}
+		log.Printf("Me: %s\nPredecessor: %s\nArgs: %s", n.me, n.predecessor, args)
 		for k, v := range(n.bucket) {
 			log.Printf("%s : %s", k, v)
 			elt := hashString(Address(k))
@@ -373,7 +387,7 @@ func (s Server) Get_all(args *Address, reply *map[Key]string) error {
 
 
 func startActor(successor Address, me Address) Server {
-  //log.Printf("succ [%v] is me [%v] ", successor, me)
+	log.Printf("startActor Me: %s", me)
 	ch := make(chan handler)
 	state := new(Node)
 	state.bucket = make(map[Key]string)
@@ -382,7 +396,10 @@ func startActor(successor Address, me Address) Server {
 		state.successor = s
 		state.successors = []Address{s}
 		client := getClient(state.successor)
-		if err := client.Call("Server.Get_all", state.me, &state.bucket); err != nil {
+		if client == nil {
+			return nil
+		}
+		if err := client.Call("Server.Get_all", me, &state.bucket); err != nil {
 			log.Fatalf("Server.Get_all: %v", err)
 		}
 		closeClient(client)
@@ -392,68 +409,53 @@ func startActor(successor Address, me Address) Server {
 	}
 	state.me = me
 
-	go func(s *Node){
+	go func(){
 		for f:= range ch {
 			f(state)
 		}
-	}(state)
-	go func(s *Node) {
+	}()
+	go func() {
 		for {
-			s.Stabilize()
-			time.Sleep(time.Second)
-		}
-	}(state)
-	go func (s *Node) {
-		for {
-			s.fix_fingers(me)
-			time.Sleep(time.Second)
-		}
-	}(state)
-	go func (s *Node) {
-		for {
-			_, err := rpc.DialHTTP("tcp", string(s.predecessor));
-			//log.Println("predecessor found")
+			state.Stabilize()
+			_, err := rpc.DialHTTP("tcp", string(state.predecessor));
 			if err != nil {
-				log.Println("lost our predecessor")
-				s.predecessor = ""
+				state.predecessor = ""
 			}
+			state.fix_fingers(me)
 			time.Sleep(time.Second)
 		}
-	}(state)
+	}()
 	return ch
 }
 
 
 
-func server (machine, port string, address Address) {
+func server (machine, port string, address Address) bool {
 	actor := startActor(address, Address(machine+":"+port))
+	if actor == nil {
+		return false
+	}
 	rpc.Register(actor)
 	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":" + port)
 	if e != nil {
-		log.Fatalf("Listen error: %v", e)
+		log.Printf("Listen error: %v", e)
+		return false
 	}
 	go func () {
 		if err := http.Serve(l, nil); err != nil {
-			log.Fatalf("http.serve: %v", err)
+			log.Printf("http.serve: %v", err)
+			return
 		}
 	}()
-}
-
-func getSpecial(address Address) *rpc.Client {
-	log.Printf("getting special client: %v", address)
-	client, err := rpc.DialHTTP("tcp", string(address))
-	if err != nil {
-		log.Fatalf("client.dialHTTP: %v", err)
-	}
-	return client
+	return true
 }
 
 func getClient(address Address) *rpc.Client {
-	// log.Printf("getting client: %v", address)
 	client, err := rpc.DialHTTP("tcp", string(address))
 	if err != nil {
-		log.Fatalf("client.dialHTTP: %v", err)
+		log.Printf("client.dialHTTP: %v", err)
+		return nil
 	}
 	return client
 }
@@ -464,20 +466,17 @@ func closeClient(c *rpc.Client) {
 	}
 }
 
-func parseFlags() (Address, string, string, error) {
+
+func main() {
 	var port string
 	var machine string
 	flag.StringVar(&port, "port", "8000", "Port that will be used to connect to chord.")
 	flag.StringVar(&machine, "machine", "localhost", "Machine that the node will be hosted on")
-	flag.Parse()
-	me := Address(machine + ":" + port)
-	return me, port, machine, nil
-}
-
-func mainloop(me Address, port string, machine string) {
 	scanner := bufio.NewScanner(os.Stdin)
+	flag.Parse()
 	listening := false
 	var junk Nothing
+	me := Address(machine + ":" + port)
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
 		switch line[0] {
@@ -490,14 +489,16 @@ func mainloop(me Address, port string, machine string) {
 			}
 		case "create":
 			listening = true
-			server(machine, port, me)
+			if server(machine, port, me) == false {
+				return
+			}
 			fmt.Println("Creating a ring on " + port)
 		case "join":
-			client := getClient(Address(line[1]))
 			listening = true
-			server(machine, port, Address(line[1]))
+			 if server(machine, port, Address(line[1])) == false {
+				 return
+			 }
 			log.Printf("join: You have joined [%s]", line[1])
-			closeClient(client)
 		case "ping":
 			client, err := rpc.DialHTTP("tcp", line[1])
 			if  err != nil {
@@ -512,7 +513,7 @@ func mainloop(me Address, port string, machine string) {
 				log.Fatalf("client.Close: %v", err)
 			}
 		case "get":
-			log.Printf("%s Hello world", hashString(Address(line[1])))
+			log.Printf("%s", hashString(Address(line[1])))
 			a := find(hashString(Address(line[1])), me)
 			client := getClient(a)
 			var reply string
@@ -523,7 +524,6 @@ func mainloop(me Address, port string, machine string) {
 			log.Printf("[%s] found [%s]", key, reply)
 			closeClient(client)
 		case "put":
-			log.Printf("Here we are at the beginning of put, here is our hash %v", hashString(Address(line[1])))
 			a := find(hashString(Address(line[1])), me)
 			args := new(BucketArgument)
 			args.Key = Key(line[1])
@@ -536,6 +536,29 @@ func mainloop(me Address, port string, machine string) {
 			}
 			log.Printf("put: [%v] => [%v]", args.Key, args.Value)
 			closeClient(client)
+		case "putrandom":
+			d, err := strconv.Atoi(line[1])
+			if err != nil {
+				log.Printf("Second argument in prutramdom is not in integer. Please use in integer")
+				break
+			}
+			for i := 0; i < d; i++ {
+				rkey := force_strings[rand.Int() % len(force_strings)]
+				rvalue := force_strings[rand.Int() % len(force_strings)]
+				a := find(hashString(Address(rkey)), me)
+				args := new(BucketArgument)
+				args.Key = Key(rkey)
+				args.Value = rvalue
+
+				log.Printf("Looking for [%s] on address [%v]", line[1], a)
+				client := getClient(a)
+				if err := client.Call("Server.Put", args, &junk); err != nil {
+					log.Fatalf("Server.Put: %v", err)
+				}
+				log.Printf("put: [%v] => [%v]", args.Key, args.Value)
+				closeClient(client)
+			}
+
 		case "delete":
 			a := find(hashString(Address(line[1])), me)
 			client := getClient(a)
@@ -545,7 +568,7 @@ func mainloop(me Address, port string, machine string) {
 			log.Printf("Found and deleted [%v]", line[2])
 			closeClient(client)
 		case "dump":
-			client := getSpecial(me)
+			client := getClient(me)
 			if err := client.Call("Server.Dump", &junk, &junk); err != nil {
 				log.Fatalf("Server.Dump: %v", err)
 			}
@@ -553,6 +576,7 @@ func mainloop(me Address, port string, machine string) {
 		case "help":
 			fmt.Println("Help goes here")
 		case "quit":
+			log.Printf("%s", me)
 			client := getClient(me)
 			var junk Nothing
 			data := make(map[Key]string)
@@ -576,14 +600,4 @@ func mainloop(me Address, port string, machine string) {
 			fmt.Println(line[0] + " is not a valid command!")
 		}
 	}
-}
-
-func main() {
-	me, port, machine, err := parseFlags()
-	if err != nil {
-    log.Panicf("Error creating node/server: %q", err)
-  }
-	finished := make(chan struct{})
-	go mainloop(me, port, machine)
-	<-finished
 }
